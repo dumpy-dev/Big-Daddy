f2d474e8b2251f04b4f3a17d03c4c4c13284b307
Chris Deutsch <chrisjdeutsch@gmail.com>
2018-03-06 21:34:58 +0000

    Calculates due date from the date of the last period and prints the result

--- Big Daddy/Controller/SettingsViewController.swift
+++ Big Daddy/Controller/SettingsViewController.swift
@@ -28,31 +28,53 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBOutlet weak var dateSwitch: UISwitch!
+    
  
     
     @IBAction func dueDatePicked(_ sender: UIDatePicker) {
         
-     // The number of days elapsed is calculated
-        let now = Date()
-        let dueDate = sender.date
+        // Dates are calculated according to either the date of the last period or the known Due Date
+        // dateSwitch isOn calculates according to last period
+        
+        if dateSwitch.isOn {
+            
+            let daysToAdd = 280
+        
+        let calculatedDueDate = Calendar.current.date(byAdding: .day, value: daysToAdd, to: sender.date)
+            
+            let now = Date()
+            let diffInDays = Calendar.current.dateComponents([.day], from: now, to: calculatedDueDate!).day
+            let weeksLeft : Int = diffInDays!/7
+            let weeksElapsed : Int = 40 - weeksLeft
+           
+            print("Your partner's due date is \(calculatedDueDate!) which means your partner is \(weeksElapsed) weeks along")
+        
+        } else {
+            
+            // This calculates the time until the baby is born from a known due date
+            
+            // The number of days elapsed is calculated
+            let now = Date()
+            let dueDate = sender.date
+            
+            let diffInDays = Calendar.current.dateComponents([.day], from: now, to: dueDate).day
+            
 
-        let diffInDays = Calendar.current.dateComponents([.day], from: now, to: dueDate).day
-        
-        print("There are \(diffInDays) days until the baby is born!")
-        
-        // The number of weeks and days is calculated from the number of days
-        
-        let weeksLeft : Int = diffInDays!/7
-        let remainderDays : Int = diffInDays!%7
-        print("You have \(weeksLeft) weeks and \(remainderDays) days to go!")
-        
-        // The number of weeks and days that have elapsed are calculated
-        
-        let weeksElapsed : Int = 40 - weeksLeft
-        let remainderDaysElapsed : Int = 7 - remainderDays
-        print("Your partner is \(weeksElapsed) weeks and \(remainderDaysElapsed) days along!")
+            // The number of weeks and days is calculated from the number of days
+            
+            let weeksLeft : Int = diffInDays!/7
+            let remainderDays : Int = diffInDays!%7
+            print("You have \(weeksLeft) weeks and \(remainderDays) days to go!")
+            
+            // The number of weeks and days that have elapsed are calculated
+            
+            let weeksElapsed : Int = 40 - weeksLeft
+            let remainderDaysElapsed : Int = 7 - remainderDays
+            print("Your partner is \(weeksElapsed) weeks and \(remainderDaysElapsed) days along!")
+            
         }
+    }
     
 
     
