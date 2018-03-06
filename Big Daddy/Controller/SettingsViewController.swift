d50d8941b8da82fe305363f8f60741af526b31e0
Chris Deutsch <chrisjdeutsch@gmail.com>
2018-03-06 20:49:35 +0000

    Calculations for days and weeks from the DatePicker are added

--- Big Daddy/Controller/SettingsViewController.swift
+++ Big Daddy/Controller/SettingsViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    var dueDate = Date()
+    var weeksElapsed : Int = 0
+    
+    @IBOutlet weak var userNameEntered: UITextField!
+    @IBOutlet weak var motherNameEntered: UITextField!
+    @IBOutlet weak var babyNameEntered: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,11 +29,34 @@ class SettingsViewController: UIViewController {
     }
     
 
-    @IBOutlet weak var userNameEntered: UITextField!
-    @IBOutlet weak var motherNameEntered: UITextField!
-    @IBOutlet weak var babyNameEntered: UITextField!
+ 
     
-    @IBOutlet weak var dueDatePicked: UIDatePicker!
+    @IBAction func dueDatePicked(_ sender: UIDatePicker) {
+        
+     // The number of days elapsed is calculated
+        let now = Date()
+        let dueDate = sender.date
+
+        let diffInDays = Calendar.current.dateComponents([.day], from: now, to: dueDate).day
+        
+        print("There are \(diffInDays) days until the baby is born!")
+        
+        // The number of weeks and days is calculated from the number of days
+        
+        let weeksLeft : Int = diffInDays!/7
+        let remainderDays : Int = diffInDays!%7
+        print("You have \(weeksLeft) weeks and \(remainderDays) days to go!")
+        
+        // The number of weeks and days that have elapsed are calculated
+        
+        let weeksElapsed : Int = 40 - weeksLeft
+        let remainderDaysElapsed : Int = 7 - remainderDays
+        print("Your partner is \(weeksElapsed) weeks and \(remainderDaysElapsed) days along!")
+        }
+    
+
+    
+    
     @IBAction func sexPicked(_ sender: UISegmentedControl) {
     
         print("Selected Segment Index is : \(sender.selectedSegmentIndex)")
@@ -35,5 +65,6 @@ class SettingsViewController: UIViewController {
         print("Selected Segment Index is : \(sender.selectedSegmentIndex)")
     }
     
+
 
 }
