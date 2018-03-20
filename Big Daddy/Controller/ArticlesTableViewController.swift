1d70a811451584fc82ca1d4274ad4345155701f0
Chris Deutsch <chrisjdeutsch@gmail.com>
2018-03-20 11:22:41 +0000

    Install Realm cocoapod

---
+++ Big Daddy/Controller/ArticlesTableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ArticlesTableViewController.swift
+//  Big Daddy
+//
+//  Created by Georgie and Chris on 14/03/2018.
+//  Copyright © 2018 Dumpy Developments. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesTableViewController: UITableViewController {
+
+    var articlesArray = [String]()
+    
+    override var prefersStatusBarHidden: Bool {
+        
+        return true
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+        
+        
+        articlesArray = ["Drinking","Foods to avoid","Morning sickness","Driving","Holidays","Babymoon","Caesarean section", "assisted delivery","vitamin K","sleep deprivation","first aid","vitamins","migraines","stretch marks","exercise","nose bleeds","preeclampsia","postnatal depression"]
+        
+        tableView.reloadData()
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return articlesArray.count
+    }
+
+override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier") as! ArticlesCustomTableViewCell
+    let text = articlesArray[indexPath.row]
+    cell.articleLabel.text = text
+    
+    if indexPath.row % 2 == 0 {
+        cell.contentView.backgroundColor = UIColor(red:0.04, green:0.41, blue:0.49, alpha:1.0)
+    } else {
+        cell.contentView.backgroundColor = UIColor(red:0.03, green:0.38, blue:0.49, alpha:1.0)
+    }
+    
+        return cell
+    }
+ 
+    
+    
+    
+    
+
+}
---
+++ Big Daddy/View/ArticlesCustomTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ArticlesCustomTableViewCell.swift
+//  Big Daddy
+//
+//  Created by Georgie and Chris on 14/03/2018.
+//  Copyright © 2018 Dumpy Developments. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesCustomTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var articleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
