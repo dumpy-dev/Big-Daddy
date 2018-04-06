51c063dd321db211d958143b384a080cc543d9ed
Chris Deutsch <chrisjdeutsch@gmail.com>
2018-04-06 21:54:41 +0100

    Collection view on the today screen is set up

---
+++ Big Daddy/View/TodayCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TodayCollectionViewCell.swift
+//  Big Daddy
+//
+//  Created by Georgie and Chris on 06/04/2018.
+//  Copyright © 2018 Dumpy Developments. All rights reserved.
+//
+
+import UIKit
+
+class TodayCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconImage: UIImageView!
+}
--- Big Daddy/View/TodayViewController.swift
+++ Big Daddy/View/TodayViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class TodayViewController: UIViewController {
+class TodayViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+
 
     // Setup the IB outlets
     @IBOutlet weak var babyAgeLabel: UILabel!
@@ -18,6 +20,8 @@ class TodayViewController: UIViewController {
     
     //Setup the arrays
     let babySizeImageArray = ["week1", "week2", "week3", "week4", "week5", "week6", "week7", "week8", "week9", "week10", "week11", "week12", "week13", "week14", "week15", "week16", "week17", "week18", "week19", "week20", "week21", "week22", "week23", "week24", "week25", "week26", "week27", "week28", "week29", "week30", "week31", "week32", "week33", "week34", "week35", "week36", "week37", "week38", "week39", "week40", "week41", "week42"]
+    
+    let iconArray = ["fetusIcon", "femaleIcon", "articlesIcon", "factIcon"]
     
     // viewDidLoad and viewWillAppear
     override func viewDidLoad() {
@@ -64,6 +68,18 @@ class TodayViewController: UIViewController {
         let weeksElapsedInt : Int = UserDefaults.standard.object(forKey: "WeeksElapsed") as! Int
         babySizeImage.image = UIImage(named: babySizeImageArray[weeksElapsedInt - 1])
         }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return iconArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "iconCell", for: indexPath) as! TodayCollectionViewCell
+        
+        cell.iconImage.image = UIImage(named:iconArray[indexPath.row])
+        
+        return cell
+    }
     
     
     override func didReceiveMemoryWarning() {
