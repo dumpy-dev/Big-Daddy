4fa5deef1e7fbe529baf0ec03e555cf69c713615
Chris Deutsch <chrisjdeutsch@gmail.com>
2018-06-20 14:00:01 +0100

    hypnobirthing programming complete

--- Big Daddy/Controller/HypnobirthingViewController.swift
+++ Big Daddy/Controller/HypnobirthingViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class HypnobirthingViewController: UIViewController {
 
+    // Setup variables and dictionary
+    
     var scriptPositionCounter = 0
-    
     let scriptDictionary : [Int:String] = [
-    
         0 : "",
-        1 : "You are getting sleepy",
-        2 : "Very. very sleepy",
+        1 : "You are feeling sleepy",
+        2 : "Very, very sleepy",
         3 : "Close your eyes",
         4 : "Drift off to sleep",
         5 : "When you wake up",
@@ -26,68 +26,65 @@ class HypnobirthingViewController: UIViewController {
         9 : "Next to you",
         10: "When I go out, well you know I'm gonna be, I'm gonna be the man",
         11 : "Who goes along with you"
-    
     ]
     
-    
-    
+    //Setup outlets
+  
     @IBOutlet var infoPopupView: UIView!
+    @IBOutlet weak var scriptLabel: UILabel!
     
-    @IBOutlet weak var visualEffectView: UIVisualEffectView!
-    var effect:UIVisualEffect!
-    
-    
-    
+    //Load the view and setup functions
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if scriptPositionCounter == 0 {
-            
             scriptPositionCounter = scriptPositionCounter + 1
-            
             UIView.animate(withDuration: 3.0, delay: 2.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
                 self.scriptLabel.alpha = 1.0
             }, completion: nil)
                 } else {
-            
             scriptLabel.text = scriptDictionary[scriptPositionCounter]
-            
             UIView.animate(withDuration: 3.0, delay: 2.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
                 self.scriptLabel.alpha = 1.0
             }, completion: nil)
-            
-            
         }
-        
-        
-        effect = visualEffectView.effect
-        visualEffectView.effect = nil
         infoPopupView.layer.cornerRadius = 5
     }
 
     
     func animateIn() {
-
         self.view.addSubview(infoPopupView)
+    
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.scriptLabel.alpha = 0.3
+        }, completion: nil)
+        
         infoPopupView.center = self.view.center
         infoPopupView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
         infoPopupView.alpha = 0
         UIView.animate(withDuration: 0.4) {
-            self.visualEffectView.effect = self.effect
+            
             self.infoPopupView.alpha = 1
             self.infoPopupView.transform = CGAffineTransform.identity
+  
         }
         
     }
    
+    // Functionality of info popup
     
-    @IBAction func closeButtonPressed(_ sender: Any) {
-        
-        
-        
+    @IBAction func infoButtonPressed(_ sender: Any) {
+        animateIn()
     }
     
-    @IBOutlet weak var scriptLabel: UILabel!
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        self.infoPopupView.removeFromSuperview()
+        UIView.animate(withDuration: 1.5, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.scriptLabel.alpha = 1.0
+        }, completion: nil)
+    }
+    
+    // Functionality of script presentation
   
     @IBAction func textTapped(_ gestureRecognizer : UITapGestureRecognizer) {
         
@@ -98,9 +95,8 @@ class HypnobirthingViewController: UIViewController {
         scriptPositionCounter = scriptPositionCounter + 1
         
         self.scriptLabel.text = scriptDictionary[scriptPositionCounter]
-        
-        // Fade in
-        UIView.animate(withDuration: 1.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+    
+        UIView.animate(withDuration: 1.5, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
             self.scriptLabel.alpha = 1.0
         }, completion: nil)
         
@@ -108,12 +104,16 @@ class HypnobirthingViewController: UIViewController {
     
     @IBAction func restartPressed(_ sender: Any) {
         
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.scriptLabel.alpha = 0.3
+        }, completion: nil)
+        
         scriptPositionCounter = 1
         
         self.scriptLabel.text = scriptDictionary[scriptPositionCounter]
         
         // Fade in
-        UIView.animate(withDuration: 1.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
             self.scriptLabel.alpha = 1.0
         }, completion: nil)
 
@@ -121,33 +121,35 @@ class HypnobirthingViewController: UIViewController {
     }
     
     @IBAction func backPressed(_ sender: Any) {
- 
+        print(scriptPositionCounter)
+        if scriptPositionCounter == 1 {
+            print("already at start")
+               self.scriptLabel.text = scriptDictionary[scriptPositionCounter]
+            
+        } else {
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.scriptLabel.alpha = 0.3
+        }, completion: nil)
+        
         scriptPositionCounter = scriptPositionCounter - 1
         
         self.scriptLabel.text = scriptDictionary[scriptPositionCounter]
         
-        // Fade in
+    
         UIView.animate(withDuration: 1.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
             self.scriptLabel.alpha = 1.0
         }, completion: nil)
-    
-    
-    
-    
+        }
     }
     
-    @IBAction func infoButtonPressed(_ sender: Any) {
-        
-        animateIn()
-        
-        
-    }
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+ 
+    
+    
 
-   
-
-}
+    }
