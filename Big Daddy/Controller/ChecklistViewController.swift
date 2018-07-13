23c12eaf69c0a89e6a101acd64d7b417ed3aa568
Chris Deutsch <chrisjdeutsch@gmail.com>
2018-07-13 21:05:16 +0100

    Checklist add popup is set up

--- Big Daddy/Controller/ChecklistViewController.swift
+++ Big Daddy/Controller/ChecklistViewController.swift
@@ -11,10 +11,12 @@ import RealmSwift
 
 class ChecklistViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var checklistTable: UITableView!
     @IBOutlet var addPopupView: UIView!
     
-    let itemArray = ["camera", "phone", "nappies", "food", "charger"]
+    var itemArray = ["camera", "phone", "nappies", "food", "charger"]
     
+    // Setup table
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -31,65 +33,65 @@ class ChecklistViewController: UIViewController, UITableViewDelegate, UITableVie
         return bagCell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let bagCell = tableView.dequeueReusableCell(withIdentifier: "checklistCell") as! HospitalBagTableViewCell
+//        bagCell.alpha = 0.1
+        print("cell selected")
+    }
+    
+    // Setup functions
     
     func animateIn() {
     self.view.addSubview(addPopupView)
-    
-    UIView.animate(withDuration: 2.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
-//    self.scriptLabel.alpha = 0.3
-    }, completion: nil)
-    
-    addPopupView.center = self.view.center
+        UIView.animate(withDuration: 0.4, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.checklistTable.alpha = 0.2
+        }, completion: nil)
+        addTextField.becomeFirstResponder()
+    addPopupView.center.x = self.view.center.x
+        addPopupView.frame.origin.y = self.view.frame.height / 4
     addPopupView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
    addPopupView.alpha = 0
     UIView.animate(withDuration: 0.4) {
-    
     self.addPopupView.alpha = 1
     self.addPopupView.transform = CGAffineTransform.identity
-    
+    }
     }
     
+    func animateOut() {
+        self.addPopupView.removeFromSuperview()
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.checklistTable.alpha = 1.0
+        }, completion: nil)
+        addTextField.resignFirstResponder()
     }
     
-    
-    
+    @IBOutlet weak var addTextField: UITextField!
     
     @IBAction func addPressed(_ sender: Any) {
-        
         animateIn()
     }
     
-
-    
     @IBAction func addCompleted(_ sender: Any) {
+        if addTextField.text != "" {
+        itemArray.append(addTextField.text!)
+            addTextField.text = ""
+            animateOut()
+            checklistTable.reloadData() }
     }
     
     @IBAction func addCancelled(_ sender: Any) {
+        animateOut()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
