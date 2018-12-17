e0075723e0acecb2c006a2eb355b59824dd3b816
Georgie and Chris <GeorgieandChris@chriss-mbp.domain.name>
2018-12-17 21:16:31 +0000

    General UI improvements

--- Big Daddy/Controller/ToolsCollectionViewController.swift
+++ Big Daddy/Controller/ToolsCollectionViewController.swift
@@ -17,15 +17,16 @@ class ToolsCollectionViewController: UICollectionViewController, UICollectionVie
     var arrayOfIDs = [String]()
     var arrayOfTypes = [String]()
     var checklistID = 0
+    var textID = 0
     
     
     //Setup array values on view loading
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        arrayOfImages = [#imageLiteral(resourceName: "sand-clock"), #imageLiteral(resourceName: "briefcase"), #imageLiteral(resourceName: "dogTag"), #imageLiteral(resourceName: "hypnosis"), #imageLiteral(resourceName: "search"), #imageLiteral(resourceName: "bodysuit"), #imageLiteral(resourceName: "rating"), #imageLiteral(resourceName: "terms")]
-        arrayOfIDs = ["Contraction Counter", "Hospital Bag", "Names", "Hypnobirthing", "Baby Decoder", "Baby Checklist", "Rate Us!", "T&Cs"]
-        arrayOfTypes = ["Timer", "Checklist", "Checklist", "General", "BabyDecoder", "BabyChecklist", "General", "General"]
+        arrayOfImages = [#imageLiteral(resourceName: "sand-clock"), #imageLiteral(resourceName: "briefcase"), #imageLiteral(resourceName: "dogTag"), #imageLiteral(resourceName: "search"), #imageLiteral(resourceName: "bodysuit"), #imageLiteral(resourceName: "rating"), #imageLiteral(resourceName: "terms"), #imageLiteral(resourceName: "credits")]
+        arrayOfIDs = ["Contraction Counter", "Hospital Bag", "Names", "Baby Decoder", "Baby Checklist", "Rate Us!", "T&Cs", "Credits"]
+        arrayOfTypes = ["Timer", "Checklist", "Checklist", "BabyDecoder", "BabyChecklist", "General", "General", "General"]
         }
 
     override func didReceiveMemoryWarning() {
@@ -64,26 +65,25 @@ func collectionView(_ collectionView: UICollectionView,
     // Setup segues for each collection view cell selection
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-
         if indexPath.row == 0 {
-            print("tool selected index path 0")
             performSegue(withIdentifier: "toolsTimerSegue", sender: nil)
         } else if indexPath.row == 1 {
-            print("tool selected index path 1")
             checklistID = 1
             performSegue(withIdentifier: "toolsChecklistSegue", sender: nil)
         } else if indexPath.row == 2 {
             checklistID = 2
             performSegue(withIdentifier: "toolsChecklistSegue", sender: nil)
         } else if indexPath.row == 3 {
-            performSegue(withIdentifier: "toolsGeneralSegue", sender: nil)
-        } else if indexPath.row == 4 {
             performSegue(withIdentifier: "decoderSegue", sender: nil)
-        } else if indexPath.row == 5 {
+        } else if indexPath.row == 4 {
             performSegue(withIdentifier: "babyChecklistSegue", sender: nil)
-        } else if indexPath.row == 6 {
+        } else if indexPath.row == 5 {
             performSegue(withIdentifier: "ratingsSegue", sender: nil)
+        } else if indexPath.row == 6 {
+            textID = 1
+            performSegue(withIdentifier: "generalTextSegue", sender: nil)
         } else if indexPath.row == 7 {
+            textID = 2
             performSegue(withIdentifier: "generalTextSegue", sender: nil)
         }
         
@@ -98,5 +98,78 @@ func collectionView(_ collectionView: UICollectionView,
             let checklistSelected : ChecklistViewController = segue.destination as! ChecklistViewController
             checklistSelected.checklistIdentifier = checklistID
         }
+        
+        if segue.identifier == "generalTextSegue" {
+            print("This is the textID: \(textID)")
+            let textSelected : TermsAndConditionsViewController = segue.destination as! TermsAndConditionsViewController
+            textSelected.textIdentifier = textID
+        }
+        
     }
+}
+
+class TermsAndConditionsViewController : UIViewController {
+    
+    var textIdentifier : Int = 0
+    @IBOutlet weak var contentField: UITextView!
+    
+    
+    override func viewDidLoad() {
+        contentField.setContentOffset(CGPoint.zero, animated: false)
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if textIdentifier == 1 {
+            contentField.text = """
+            
+                Medical information disclaimer
+            
+            1.    Credit
+            1.1    This document was created using a template from SEQ Legal (https://seqlegal.com).
+            
+            2.    No advice
+            2.1    Our app contains general medical information.
+            2.2    The medical information is not advice and should not be treated as such.
+            
+            3.    No warranties
+            3.1    The medical information on our app is provided without any representations or warranties, express or implied.
+            3.2    Without limiting the scope of Section 3.1, we do not warrant or represent that the medical information on this app:
+            (a)    will be constantly available, or available at all; or
+            (b)    is true, accurate, complete, current or non-misleading.
+            
+            4.    Medical assistance
+            4.1    You must not rely on the information on our app as an alternative to medical advice from your doctor or other professional healthcare provider.
+            4.2    If you have any specific questions about any medical matter, you should consult your doctor or other professional healthcare provider.
+            4.3    If you think you may be suffering from any medical condition, you should seek immediate medical attention.
+            4.4    You should never delay seeking medical advice, disregard medical advice or discontinue medical treatment because of information on our app.
+            
+            5.    Limits upon exclusions of liability
+            5.1    Nothing in this disclaimer will:
+            (a)    limit or exclude any liability for death or personal injury resulting from negligence;
+            (b)    limit or exclude any liability for fraud or fraudulent misrepresentation;
+            (c)    limit any liabilities in any way that is not permitted under applicable law; or
+            (d)    exclude any liabilities that may not be excluded under applicable law.
+            """
+        } else if textIdentifier == 2 {
+            contentField.text = """
+            
+            Image Credits
+            
+            Icons made by:
+            - Freepik
+            - Smashicon
+            - Pixel Perfect
+            - Pixel Buddha
+            - Roundicons
+            
+            from www.flaticon.com
+
+            """
+        } else {
+            contentField.text = ""
+        }
+    }
+    
+    
 }
